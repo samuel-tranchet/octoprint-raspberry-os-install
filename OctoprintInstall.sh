16a928dac1377b9f8562388b953cb96d5ca71b9c
Samuel Tranchet <samuel.tranchet@unity3d.com>
2021-11-26 02:05:27 +0100

    Final urls OK

--- OctoprintInstall.sh
+++ OctoprintInstall.sh
@@ -15,7 +15,7 @@ pip install --no-cache-dir octoprint
 sudo usermod -a -G tty pi
 sudo usermod -a -G dialout pi
 
-wget 
+wget https://raw.githubusercontent.com/samuel-tranchet/octoprint-raspberry-pi-os-install/main/etc/systemd/system/octoprint-server.service
 sudo mv octoprint-server.service /etc/systemd/system/octoprint-server.service
 
 
@@ -33,10 +33,10 @@ make
 cd ~/OctoPrint
 
 
-wget 
+wget https://raw.githubusercontent.com/samuel-tranchet/octoprint-raspberry-pi-os-install/main/home/pi/OctoPrint/octoprint-webcam-start.sh
 chmod +x octoprint-webcam-start.sh
 
-wget 
+wget https://raw.githubusercontent.com/samuel-tranchet/octoprint-raspberry-pi-os-install/main/etc/systemd/system/octoprint-webcam.service
 sudo mv octoprint-webcam.service /etc/systemd/system/octoprint-webcam.service
 
 
